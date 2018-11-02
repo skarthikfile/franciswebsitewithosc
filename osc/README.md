fc82334a267d453d5bd7e69b5a794e5a836afcb2
skarthikfile <skarthikfile@gmail.com>
2018-11-02 02:40:43 -0700

    'new'

--- osc/README.md
+++
@@ -1,6 +0,0 @@
-osCommerce Online Merchant v2.x
-===============================
-
-Website: http://www.oscommerce.com
-
-Support Forums: http://forums.oscommerce.com
--- osc/catalog/includes/modules/content/checkout_success/templates/product_notifications.php
+++
@@ -1,7 +0,0 @@
-<div class="contentText">
-  <?php echo MODULE_CONTENT_CHECKOUT_SUCCESS_PRODUCT_NOTIFICATIONS_TEXT_NOTIFY_PRODUCTS; ?>
-
-  <p class="productsNotifications">
-    <?php echo $products_notifications; ?>
-  </p>
-</div>
